29c779158c3317075489accd59f1e0c588cddab0
msko <kms89123@gmail.com>
2022-02-28 23:50:16 +0900

    Password bcryptjs

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,6 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { AuthCredentialsDto } from './dto/auth.dto';
 import { User } from './user.entity';
 
@@ -10,7 +11,10 @@ import { User } from './user.entity';
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
